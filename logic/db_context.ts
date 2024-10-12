7df9694250532e0db2f1ae93d02a6df6956ba0ed
BeneX Drake <benex@hotmail.de>
2024-10-12 10:26:20 +0200

    add sql api for learning

---
+++ logic/db_context.ts
@@ -0,0 +1,12 @@
+import { Database } from 'sqlite3';
+
+export class DbContext
+{
+    public db:Database | undefined;
+
+    constructor()
+    {
+        this.db = new Database('techstarter.db')
+    }
+
+}
---
+++ pages/api/sql.ts
@@ -0,0 +1,44 @@
+import type { NextApiRequest, NextApiResponse } from "next";
+import { DbContext } from "@/logic/db_context";
+
+export default async function handler(req:NextApiRequest,res:NextApiResponse)
+{
+    let db_context = new DbContext()
+
+    let query:string = req.body.query;
+    let pw:string = req.body.pw;
+
+    if(!pw)
+    {
+        res.status(200).json({'error':'You should not pass!!!'})
+        return
+    }
+    
+    if(pw !== process.env.password)
+    {
+        res.status(200).json({'error':'Wrong Password'})
+        return
+    }
+
+    if(!query)
+    {
+        res.status(200).json({'error':'Where is the SQL Query?'})
+        return
+    }
+
+    if (query.toLowerCase().includes('drop'))
+    {
+        res.status(200).json({'error':'Drop is forbidden!'})
+        return
+    }
+    
+    if(query === '')
+        return {'error':'Empty Query'}
+    
+    db_context.db?.all(query, (err: any, result: any) => {
+            if (result.length > 0)
+                res.status(200).json({'result': result})
+            else if (err)
+                res.status(200).json({'error': err.message})
+        });
+}
