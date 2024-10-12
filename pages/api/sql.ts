ae0940b94c0216a1e99a0a4744a78538377e700b
BeneX Drake <benex@hotmail.de>
2024-10-12 11:37:07 +0200

    its works yeah

--- pages/api/sql.ts
+++ pages/api/sql.ts
@@ -8,6 +8,8 @@ export default async function handler(req:NextApiRequest,res:NextApiResponse)
     let query:string = req.query.query+"";
     let pw:string = req.query.pw+"";
 
+    
+
     if(!pw)
     {
         res.status(200).json({'error':'You should not pass!!!'})
